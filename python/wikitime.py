005909c408f0a12936e15ccc60af7c53f58192f6
steve <ssosik@akamai.com>
2019-03-10 14:24:03 -0400

    Following along with the vim-python tutorial

---
+++ python/wikitime.py
@@ -0,0 +1,6 @@
+# Implement Markdown handling tools similar to what Vimwiki offers. Code here is
+# following the example written here:
+# http://candidtim.github.io/vim/2017/08/11/write-vim-plugin-in-python.html
+
+def foo():
+    print("FOO BAR")
